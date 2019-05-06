61dc2e3bb14f4aa75c51f0d0bbe6c51568dede14
Brazen <764146802@qq.com>
2019-05-07 00:25:52 +0800

    第一个组件

---
+++ src/pages/index/index.js
@@ -0,0 +1,20 @@
+import 'css/common.css'
+import './index.css'
+import Vue from 'vue'
+import url from 'js/api.js'
+import axios from 'axios'
+
+let app = new Vue({
+    el:'#app',
+    data:{
+        lists:null,
+    },
+    created() {
+        axios.get(url.hotLists,{
+            pageNum:1,
+            pageSize:6
+        }).then(res=>{
+            this.lists = res.data.lists
+        })
+    },
+})
