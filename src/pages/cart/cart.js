e8cd7f0293949e10f38f28a9be496cc9ea9e6d6a
Brazen <764146802@qq.com>
2019-05-12 23:24:59 +0800

    部分店铺内容

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -13,6 +13,19 @@ new Vue({
     lists:null
   },
   computed:{
+    allSelected:{
+      get(){
+        if(this.lists&&this.lists.length){
+          return this.lists.every(shop=>{
+            return shop.checked
+          })
+        }
+        return false
+      },
+      set(){
+
+      }
+    }
   },
   created(){
     this.getList()
@@ -20,13 +33,24 @@ new Vue({
   methods:{
     getList(){
       axios.get(url.cartLists).then(res => {
-        this.lists = res.data.cartList
-        this.lists.forEach(shop=>{
-          shop.goodsList.forEach(good => {
+        let lists = res.data.cartList
+        lists.forEach(shop=>{
+          shop.checked = true
+          shop.goodsList.forEach(good=>{
             good.checked = true
           })
         })
+        this.lists = lists
       })
+    },
+    selectGood(shop,good){
+      good.checked = !good.checked
+      shop.checked = shop.goodsList.every(good=>{
+        return good.checked
+      })
+    },
+    selectShop(shop){
+      shop.checked = !shop.checked
     }
   },
   filters:{
