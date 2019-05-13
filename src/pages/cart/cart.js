1a6e5c03bc689d6ba738f995cd1241a17f8f8d7b
Brazen <764146802@qq.com>
2019-05-13 19:34:41 +0800

    删除

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -11,7 +11,9 @@ new Vue({
   el:'.container',
   data:{
     lists:null,
-    total:0
+    total:0,
+    editingShop:null,
+    editingShopIndex:-1
   },
   computed:{
     allSelected:{
@@ -32,6 +34,23 @@ new Vue({
         })
       }
     },
+    allRemoveSelected:{
+      get(){
+        if(this.editingShop){
+          return this.editingShop.removeChecked
+        }
+        return false
+      },
+      set(newVal){
+        if(this.editingShop){
+          this.editingShop.removeChecked = newVal
+          this.editingShop.goodsList.forEach(good=>{
+            good.removeChecked = newVal
+          })
+        }
+      }
+    },
+    // 这是为了之后准备
     selectLists(){
       if(this.lists&&this.lists.length){
         let arr = []
@@ -48,6 +67,18 @@ new Vue({
         return arr
       }
       return []
+    },
+    removeLists(){
+      if(this.editingShop){
+        let arr = []
+        this.editingShop.goodsList.forEach(good=>{
+          if(good.removeChecked){
+            arr.push(good)
+          }
+        })
+        return arr
+      }
+      return []
     }
   },
   created(){
@@ -59,29 +90,34 @@ new Vue({
         let lists = res.data.cartList
         lists.forEach(shop=>{
           shop.checked = true
+          shop.removeChecked = false
           shop.editing = false
           shop.editingMsg = '编辑'
           shop.goodsList.forEach(good=>{
             good.checked = true
+            good.removeChecked = false
           })
         })
         this.lists = lists
       })
     },
     selectGood(shop,good){
-      good.checked = !good.checked
-      shop.checked = shop.goodsList.every(good=>{
-        return good.checked
+      let attr = this.editingShop ? 'removeChecked' : 'checked'
+      good[attr] = !good[attr]
+      shop[attr] = shop.goodsList.every(good=>{
+        return good[attr]
       })
     },
     selectShop(shop){
-      shop.checked = !shop.checked
+      let attr = this.editingShop ? 'removeChecked' : 'checked'
+      shop[attr] = !shop[attr]
       shop.goodsList.forEach(good=>{
-        good.checked = shop.checked
+        good[attr] = shop[attr]
       })
     },
     selectAll(){
-      this.allSelected = !this.allSelected
+      let attr = this.editingShop ? 'allRemoveSelected' : 'allSelected'
+      this[attr] = !this[attr]
     },
     edit(shop,shopIndex){
       shop.editing = !shop.editing
@@ -91,6 +127,26 @@ new Vue({
           item.editing = false
           item.editingMsg = shop.editing ? '':'编辑'
         }
+      })
+      this.editingShop = shop.editing ? shop : null
+      this.editingShopIndex = shop.editing ? shopIndex : -1
+    },
+    add(good){
+      axios.post(url.addCart,{
+        id:good.id,
+        number:1
+      }).then(res=>{
+        good.number++
+      })
+
+    },
+    reduce(good){
+      if(good.number===1)return
+      axios.post(url.cartReduce,{
+        id:good.id,
+        number:1
+      }).then(res=>{
+        good.number--
       })
     }
   },
