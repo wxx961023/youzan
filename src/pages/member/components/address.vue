6131debc156c4bcd9f13bf04e4c3b60eb84b1a40
Brazen <764146802@qq.com>
2019-05-15 23:36:01 +0800

    ’路由1‘

---
+++ src/pages/member/components/address.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    地址管理
+  </div>
+</template>
+
+
+<style scoped>
+  @import './address.css';
+  @import './address_base.css';  
+</style>
+
+<!--入口在member.html那里-->
--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,3 +1,26 @@
-import './member_base.css'
-import './member.css'
 
+//1.使用vue-router
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+
+let routes = [{
+  path:'/',
+  components: require('./components/member.vue')
+},
+{
+  path:'/address',
+  components: require('./components/address.vue')
+}]
+
+//创建router实例
+
+let router = new Router({
+  routes
+})
+
+//根组件注入
+new Vue({
+  el:'#app',
+  router
+})
